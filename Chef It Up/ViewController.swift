3ab8a3050e4e2e1e6334972c7721229169bb8358
Jay-son Alburg <Jay-son@Jay-sons-MacBook-Pro.local>
2019-04-09 19:20:59 -0400

    initial project commit

---
+++ Chef It Up/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chef It Up
+//
+//  Created by Jay-son Alburg on 4/9/19.
+//  Copyright © 2019 Jay'son Alburg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Chef It UpTests/Chef_It_UpTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chef_It_UpTests.swift
+//  Chef It UpTests
+//
+//  Created by Jay-son Alburg on 4/9/19.
+//  Copyright © 2019 Jay'son Alburg. All rights reserved.
+//
+
+import XCTest
+@testable import Chef_It_Up
+
+class Chef_It_UpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
